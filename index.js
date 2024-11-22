47740ba78c81d46d37abcdac5e3b12ec0a77397f
Пользователь <i.n.kustov@gmail.com>
2024-11-22 16:39:49 +0500

    server is running

--- index.js
+++ index.js
@@ -1,59 +1,31 @@
 const http = require("http");
+const helper = require("./helper");
+const readAll = require("./handlers/readall");
 
 const HOST = "127.0.0.1";
 const PORT = 3000;
-const HANDLERS = {
-  "/sum": sum,
+
+const endpointMapper = {
+  "/readall": readAll,
 };
 
-const server = http.createServer((request, response) => {
-  parseBodyJson(request, (error, payload) => {
-    const handler = getHandler(request.url);
-
-    handler(request, response, payload, (error, result) => {
-      if (error) {
-        response.statusCode = error.code;
-        response.setHeader("Content-Type", "application/json");
-        response.end(JSON.stringify(error));
-
-        return;
-      }
-
-      response.statusCode = 200;
-      response.setHeader("Content-Type", "application/json");
-      response.end(JSON.stringify(result));
-    });
-  });
-});
+const server = http.createServer(handler);
 
 server.listen(PORT, HOST, () => {
   console.log(`Server running at http://${HOST}:${PORT}/`);
 });
 
-function getHandler(url) {
-  return HANDLERS[url] || notFound;
+function handler(req, res) {
+  const controller = endpointMapper[req.url];
+
+  if (controller) {
+    controller(req, res, params);
+  } else {
+    notFound(req, res);
+  }
 }
 
-function sum(request, response, payload, cb) {
-  const result = { c: payload.a + payload.b };
-
-  cb(null, result);
-}
-
-function notFound(request, response, payload, cb) {
-  cb({ code: 404, message: "Not found" });
-}
-
-function parseBodyJson(request, cb) {
-  let body = [];
-
-  request
-    .on("data", (chunk) => {
-      body.push(chunk);
-    })
-    .on("end", () => {
-      body = Buffer.concat(body).toString();
-      let params = JSON.parse(body);
-      cb(null, params);
-    });
+function notFound(req, res) {
+  res.statusCode = 404;
+  res.end("Not Found");
 }
