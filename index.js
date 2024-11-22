2edcbb909ec641e912fede4209f1ca3faf8d98ff
Пользователь <i.n.kustov@gmail.com>
2024-11-22 15:00:13 +0500

    C:/Program Files/Git/sum rout implemented

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
 const http = require("http");
 
-const hostname = "127.0.0.1";
-const port = 3000;
+const HOST = "127.0.0.1";
+const PORT = 3000;
+const HANDLERS = {
+  "/sum": sum,
+};
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+const server = http.createServer((request, response) => {
+  parseBodyJson(request, (error, payload) => {
+    const handler = getHandler(request.url);
+
+    handler(request, response, payload, (error, result) => {
+      if (error) {
+        response.statusCode = error.code;
+        response.setHeader("Content-Type", "application/json");
+        response.end(JSON.stringify(error));
+
+        return;
+      }
+
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "application/json");
+      response.end(JSON.stringify(result));
+    });
+  });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}/`);
 });
+
+function getHandler(url) {
+  return HANDLERS[url] || notFound;
+}
+
+function sum(request, response, payload, cb) {
+  const result = { c: payload.a + payload.b };
+
+  cb(null, result);
+}
+
+function notFound(request, response, payload, cb) {
+  cb({ code: 404, message: "Not found" });
+}
+
+function parseBodyJson(request, cb) {
+  let body = [];
+
+  request
+    .on("data", (chunk) => {
+      body.push(chunk);
+    })
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+      let params = JSON.parse(body);
+      cb(null, params);
+    });
+}
